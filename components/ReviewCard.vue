6138ce4f260adaafaffe8b82278ed7e16fcb717d
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-05-07 12:35:01 +0500

    change

--- components/ReviewCard.vue
+++ components/ReviewCard.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <div class="p-6 md:p-8 bg-[#191818c4] mt-6 rounded-lg text-center md:text-left h-full">
     <NuxtImg :src="review.acf.image" alt="review star" class="max-w-[160px] mx-auto md:ml-0 md:mr-auto" />
     <p class="mt-8 text-lg text-white font-bold line-clamp-1">{{ review.acf.title }}</p>
@@ -7,6 +8,7 @@
       <NuxtLink :to="review.acf?.action || '/#'" target="_blank" external>See more</NuxtLink>
     </p>
   </div>
+
 </template>
 
 <script setup>
